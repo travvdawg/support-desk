9c836bfcc149547d7e7c6888fc1a8b0504a62081
Your Name <olivertravis554@gmail.com>
2024-02-13 20:14:55 -0500

    mongodb and servers setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middlewear/errorMiddlewear');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+// connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Welcome to the support desk api' });
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
+});
